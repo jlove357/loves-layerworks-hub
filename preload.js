909b280f2b39e218b67fee07d20d2b5ff094a18b
jlove357 <joshua.love357@gmail.com>
2026-08-05 22:58:13 -0400

    Stage M1B preload bridge

--- preload.js
+++ preload.js
@@ -2,8 +2,17 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('layerWorks', {
   appName: "Love's LayerWorks Hub",
-  milestone: 'M1A',
+  milestone: 'M1B',
   loadData: () => ipcRenderer.invoke('hub:load-data'),
   saveData: (data) => ipcRenderer.invoke('hub:save-data', data),
-  dataPath: () => ipcRenderer.invoke('hub:data-path')
+  dataStatus: () => ipcRenderer.invoke('hub:data-status'),
+  selectSwatchImage: () => ipcRenderer.invoke('hub:select-swatch-image'),
+  copySwatchImage: (request) => ipcRenderer.invoke('hub:copy-swatch-image', request),
+  readManagedImage: (relativePath) => ipcRenderer.invoke('hub:read-managed-image', relativePath),
+  deleteManagedImage: (relativePath) => ipcRenderer.invoke('hub:delete-managed-image', relativePath),
+  exportInventory: () => ipcRenderer.invoke('hub:export-inventory'),
+  importInventory: () => ipcRenderer.invoke('hub:import-inventory'),
+  listBackups: () => ipcRenderer.invoke('hub:list-backups'),
+  restoreBackup: (fileName) => ipcRenderer.invoke('hub:restore-backup', fileName),
+  exportFullBackup: () => ipcRenderer.invoke('hub:export-full-backup')
 });
