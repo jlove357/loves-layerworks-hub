37ca7e71c6bfa465acec4318dec430969fb226c3
jlove357 <joshua.love357@gmail.com>
2026-08-06 13:19:42 -0400

    Expose Gallery Catalog image bridge

--- preload.js
+++ preload.js
@@ -2,14 +2,16 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('layerWorks', {
   appName: "Love's LayerWorks Hub",
-  milestone: 'M3A',
+  milestone: 'M4A',
   loadData: () => ipcRenderer.invoke('hub:load-data'),
   saveData: (data) => ipcRenderer.invoke('hub:save-data', data),
   dataStatus: () => ipcRenderer.invoke('hub:data-status'),
   selectSwatchImage: () => ipcRenderer.invoke('hub:select-swatch-image'),
   selectProjectImage: () => ipcRenderer.invoke('hub:select-project-image'),
+  selectFinishedImage: () => ipcRenderer.invoke('hub:select-finished-image'),
   copySwatchImage: (request) => ipcRenderer.invoke('hub:copy-swatch-image', request),
   copyProjectImage: (request) => ipcRenderer.invoke('hub:copy-project-image', request),
+  copyFinishedImage: (request) => ipcRenderer.invoke('hub:copy-finished-image', request),
   readManagedImage: (relativePath) => ipcRenderer.invoke('hub:read-managed-image', relativePath),
   deleteManagedImage: (relativePath) => ipcRenderer.invoke('hub:delete-managed-image', relativePath),
   exportInventory: () => ipcRenderer.invoke('hub:export-inventory'),
